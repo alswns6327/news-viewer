40b75a5b65ca54672a394e813f8ff4242c68af7c
alswns6327 <alswnsdl333@gmail.com>
2023-11-16 18:31:11 +0900

    axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from '../node_modules/axios/index';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const onClick = async () => {
+    try{
+      
+      let response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+  
+      setData(response.data);
+    }catch(e){
+      console.log(e);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={onClick}>읽어오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
     </div>
   );
 }
