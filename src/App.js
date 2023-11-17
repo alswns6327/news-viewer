18e00cd789946a4d4830293c567f148683fc1868
alswns6327 <alswnsdl333@gmail.com>
2023-11-18 00:03:47 +0900

    list

--- src/App.js
+++ src/App.js
@@ -2,51 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { useCallback, useRef, useState } from 'react';
 import axios from '../node_modules/axios/index';
+import NewsList from './NewsList';
+import Categories from './components/Categories';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  const [category, setCategory] = useState(["", "business", "entertainment", "general", "health", "science", "sports", "technology"]);
-
-  let currentCategory = useRef("");
-
-  const onClick = async () => {
-    try{
-      
-
-      //let response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
-  
-      let response;
-      if(currentCategory){
-        response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr&category=${currentCategory.current}&apiKey=70414065b47b4dedb2823a8b089b9a1b`);
-      }else{
-        response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=70414065b47b4dedb2823a8b089b9a1b`);
-      }
-
-      setData(response.data);
-    }catch(e){
-      console.log(e);
-    }
-  }
-
-  const categoryChange = useCallback(e => {
-    currentCategory = e.target.value;
-  }, []);
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
 
   return (
     <div className="App">
-      <div>
-        <button onClick={onClick}>뉴스 불러오기</button>
-      </div>
-      <ul>
-        {category.map(ca => (<li key={ca}>
-          <label>
-            <input type='radio' name='category' onChange={categoryChange} value={ca}/>
-            {ca}
-          </label>
-        </li>))}
-      </ul>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
+      <Categories onSelect={onSelect} category={category}/>
+      <NewsList category={category}/>
     </div>
   );
 }
---
+++ src/NewsList.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from '../node_modules/axios/index';
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+    @media screen and (max-width: 768px){
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+`
+
+const sampleArticle = {
+    title : '제목',
+    description : '내용',
+    url : 'https://google.com',
+    urlToImage: 'https://via.placeholder.com/160'
+};
+
+const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try{
+                const response = await axios.get("https://newsapi.org/v2/top-headlines?country=kr&apiKey=70414065b47b4dedb2823a8b089b9a1b");
+                setArticles(response.data.articles)
+            }catch(e){
+                console.log(e);
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, [])
+
+    if(loading){
+        return <NewsListBlock>대기 중...</NewsListBlock>
+    }
+
+    if(!articles){
+        return null;
+    }
+
+    return (
+        <NewsListBlock>
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article}/>
+            ))}
+        </NewsListBlock>
+    );
+};
+
+export default NewsList;
